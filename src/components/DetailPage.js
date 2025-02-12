ebd663e8ea2553c11f2b1a2d3e72a447d5b2f750
Satyajit-JDC <snarkhede@jdc-group.com>
2025-02-12 12:09:48 +0530

    v2.0

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import {
-    Title, Tag, MessageStrip, Timeline, TimelineItem, List, ListItemStandard, FlexBox, AnalyticalTable, ObjectPageHeader, ObjectPageSubSection, ObjectPage, ObjectPageTitle, Toolbar, Text, Link, Label, FormItem, ObjectPageSection, Form, ToolbarButton, ObjectStatus
+     RatingIndicator, NotificationList, NotificationListItem, Title, Tag, MessageStrip, Timeline, TimelineItem, List, ListItemStandard, FlexBox, AnalyticalTable, ObjectPageHeader, ObjectPageSubSection, ObjectPage, ObjectPageTitle, Toolbar, Text, Link, Label, FormItem, ObjectPageSection, Form, ToolbarButton, ObjectStatus
 } from '@ui5/webcomponents-react';
 
+
 import { useLocation, useNavigate } from "react-router-dom";
+
+import {PieChart}from '@ui5/webcomponents-react-charts';
 
 import axios from "axios";
 import { useIntl } from 'react-intl';
@@ -94,7 +97,7 @@ const DetailPage = () => {
                     height: '91vh'
                 }}
                 titleArea={<ObjectPageTitle actionsBar={<Toolbar design="Transparent"></Toolbar>} header="Satyajit N Narkhede" subHeader="SAP BTP Architect | Fiori | CAPM | ReactJS "><ObjectStatus state="Positive">#OPENTOWORK</ObjectStatus></ObjectPageTitle>}
-
+headerPinned
             >
                 <ObjectPageSection
                     aria-label={intl.formatMessage({ id: 'Detail.SUMMARY' })}
@@ -230,6 +233,48 @@ const DetailPage = () => {
                     aria-label={intl.formatMessage({ id: 'Detail.CERTIFICATIONS' })}
                     id="Certifications"
                     titleText={intl.formatMessage({ id: 'Detail.CERTIFICATIONS' })} >
+<NotificationList>
+
+<NotificationListItem
+    footnotes={<><Label>Issued by SAP on Sep,2023
+    </Label></>}
+    importance="Standard"
+    titleText="SAP Certified Development Associate  SAP BT
+Extensions with SAP Cloud Application
+Programming Model
+"
+    wrappingType="Normal"
+>
+
+</NotificationListItem>
+
+<NotificationListItem
+    footnotes={<><Label>Issued by SAP on Nov, 2023
+
+    </Label></>}
+    importance="Standard"
+    titleText="SAP Certified Development Associate  Side-by-Side Extensibility Based on SAP BTP- Kyma Runtime"
+    wrappingType="Normal"
+>
+
+</NotificationListItem>
+
+<NotificationListItem
+    footnotes={<><Label>Issued by SAP on Apr,2024
+
+    </Label></>}
+    importance="Standard"
+    titleText="Creating Applications and Extensions using SAP
+Build Code  Record of Achievement
+"
+    wrappingType="Normal"
+>
+
+</NotificationListItem>
+
+</NotificationList>
+
+                        
 
                 </ObjectPageSection>
                 <ObjectPageSection
@@ -243,6 +288,7 @@ const DetailPage = () => {
                             name="Lead Consultant- [SAP UX & BTP Application Architect]"
                             subtitleText="Bosch Global Software Technologies Pvt Ltd  - Pune, India"
                             titleText="09/2023 - Present"
+                            nameClickable
                         >
                             <div>Working as SAP BTP application Architect and Lead</div>
                             <div>Responsible for delivery of Side-by-Side Extension objects</div>
@@ -256,6 +302,7 @@ const DetailPage = () => {
                             name="Manager [SAP UX & BTP Application Architect]"
                             subtitleText="Deloitte USI -  Mumbai, India/Tokyo, Japan"
                             titleText="06/2018 - 09/2023"
+                            nameClickable
                         >
                             <div>Worked as SAP BTP Development Architect</div>
                             <div>Responsible for design and implementation of Fiori UI/UX</div>
@@ -273,6 +320,7 @@ const DetailPage = () => {
                             name="Senior Software Engineer [SAP Fiori and SAP Workflow]"
                             subtitleText="Zensar Technologies -  Mumbai, India"
                             titleText="09/2016 - 05/2018"
+                            nameClickable
                         >
                             <div>Responsible for the delivery of Fiori based Human Capital Management</div>
                             <div>Objects/Applications Development of configuration various workflows in
@@ -285,6 +333,7 @@ const DetailPage = () => {
                             name="SAP Workflow Analyst [SAP Fiori and SAP Workflow]"
                             subtitleText="Systemax -  Mumbai, India"
                             titleText="10/2015  09/2016"
+                            nameClickable
                         >
                             <div>Involved in the development of SAP Workflow for Procurement and Human
                                 Capital Management modules</div>
@@ -297,6 +346,7 @@ const DetailPage = () => {
                             name="Software Engineer [SAP ABAP and SAP Fiori ]"
                             subtitleText="CMC Limited -  Mumbai, India"
                             titleText="08/2012  08/2015 "
+                            nameClickable
                         >
                             <div>Worked as SAP technical consultant for ABAP developments</div>
 
@@ -315,19 +365,180 @@ const DetailPage = () => {
                         aria-label={intl.formatMessage({ id: 'Detail.KEYPROJECTS' })}
                         id="Key Projects"
                         titleText={intl.formatMessage({ id: 'Detail.KEYPROJECTS' })}
-                    ></ObjectPageSubSection>
+                    >
+                        <NotificationList>
+                            <NotificationListItem
+                                footnotes={<><Label>American agribusiness cooperative owned by farmers and member
+                                    cooperatives</Label><Label>01/2024 - Present</Label></>}
+                                importance="Standard"
+                                titleText="SAP BTP Product Development"
+                                wrappingType="Normal"
+                            >
+                                Offshore SAP BTP lead for product
+                                development.
+                                <br />
+                                Quality lead for CAPM and SAP Fiori Applications(UI5 and React JS)
+                                <br />
+                                Responsible for development of React JS application along with UI5 web
+                                component
+                                <br />
+                                Mentoring resources for CAPM and SAP BTP
+                                <br />
+                                Responsible for setting up development environment - VScode [Githu
+                                copilot, BAS toolkit], Event Mesh, CICD
+                                <br />
+                                Involved in development of Dashboard for RINS soultion using React JS.
+
+                            </NotificationListItem>
+                            <NotificationListItem
+                                footnotes={<><Label>A German company engine manufacturing brands</Label><Label>09/2023 - 12/2023</Label></>}
+                                importance="Standard"
+                                titleText="SAP BTP Side-by-Side Extensions with S/4 HANA Public Cloud"
+                                wrappingType="Normal"
+                            >
+                                Involved Development of Side-by-Side extension like development of report
+                                and transactional application on BTP consuming standard API and custom API
+                                build in S/4 HANA public cloud
+                                <br />
+                                Development of custom report for work item status for workflows running in
+                                process automation.[using CAP and Fiori Element UI]
+
+
+
+                            </NotificationListItem>
+
+                            <NotificationListItem
+                                footnotes={<><Label>A Japanese Semiconductor Solutions Group is a corporate group that develops
+                                    Imaging and Sensing solution business</Label><Label>07/2022 - 09/2023</Label></>}
+                                importance="Standard"
+                                titleText="SAP BTP Side-by-Side Extensions with S/4 HANA on Premises"
+                                wrappingType="Normal"
+                            >
+                                Worked as SAP BTP Development Architect- Responsible for overal
+                                solutions in BTP cloud foundry environment
+                                <br />
+                                Responsible to provide Technical Workshops to client on various development
+                                methods
+                                <br />
+                                Responsible for development cataloging with respect to Classical, In-App,
+                                Side-by-Side Extensibility
+                                <br />
+                                Responsible for development and delivery of SAP BTP objects (CAP with
+                                custom authorization and dynamic attributes, FIORI- SAPUI5)
+                                <br />
+                                Involved in configuration of GitLab flow for source code management and
+                                transport of BTP objects to next subaccount
+
+
+
+
+                            </NotificationListItem>
+                            <NotificationListItem
+                                footnotes={<><Label>Electric power company in Zurich, Switzerland</Label><Label>02/2021 - 07/2022</Label></>}
+                                importance="Standard"
+                                titleText="SAP Fiori on BTP with S/4 HANA on-Premises"
+                                wrappingType="Normal"
+                            >
+                                Led Fiori Development team
+                                <br />
+                                Responsible for setting up SAP Business Application studio
+                                <br />
+                                Successfully delivered Fiori First solution (130+ Custom Fiori Apps)
+                                <br />
+                                Co-ordinating with client technical solution architect for overall progress of
+                                project
+                                <br />
+                                Responsible for providing solution to complex scenario
+
+
+
+
+                            </NotificationListItem>
+
+                            <NotificationListItem
+                                footnotes={<><Label>An American manufacturer of ammunition, chlorine, and sodium hydroxide</Label><Label>07/2018 - 01/2021</Label></>}
+                                importance="Standard"
+                                titleText="SAP Fiori Implementation on S/4 HANA on Premises"
+                                wrappingType="Normal"
+                            >
+                                Worked as Team lead for Delivery of RICEFW objects in Transportation
+                                Management, Production Planning, Quality Management
+                                <br />
+                                Responsible for daily project status reporting to the management
+
+
+
+
+
+                            </NotificationListItem>
+
+                        </NotificationList>
+
+
+                    </ObjectPageSubSection>
 
                     <ObjectPageSubSection
                         aria-label={intl.formatMessage({ id: 'Detail.PASTPROJECTS' })}
                         id="Past Projects"
                         titleText={intl.formatMessage({ id: 'Detail.PASTPROJECTS' })}
-                    ></ObjectPageSubSection>
+                    >
+                        <NotificationList>
+
+                            <NotificationListItem
+                                footnotes={<><Label>CIDCO (City And Industrial Development Corporation), Systemax, AER
+                                    (Atomic Energy Regulatory Board) and Shared Support
+                                </Label><Label>08/2012 - 06/2018</Label></>}
+                                importance="Standard"
+                                titleText="CIDCO (City And Industrial Development Corporation), Systemax, AER
+(Atomic Energy Regulatory Board) and Shared Support
+"
+                                wrappingType="Normal"
+                            >
+                                Responsible for creation UI5 Application for Purchase requisition and
+                                Purchase Order approval
+                                <br />
+                                Involved creation of Workflows in MM module (PO, PR, SES)
+                                <br />
+                                Customization of workflows in Human Resource module
+                                <br />
+                                Responsible for development of Workflow for Purchase Requisition, Purchase
+                                Order, Goods Receipt
+                                <br />
+                                Responsible for development of BDC program for maintenance address
+                                details for Vendor(Call Transaction)
+
+
+
+
+
+
+
+                            </NotificationListItem>
+
+                        </NotificationList>
+                    </ObjectPageSubSection>
 
                 </ObjectPageSection>
                 <ObjectPageSection
                     aria-label={intl.formatMessage({ id: 'Detail.EDUCATION' })}
                     id="Education"
                     titleText={intl.formatMessage({ id: 'Detail.EDUCATION' })} >
+
+<NotificationList>
+
+<NotificationListItem
+    footnotes={<><Label>Mumbai University, India
+    </Label><Label>09/2008 - 05/2012</Label></>}
+    importance="Standard"
+    titleText="Bachelor of Engineering"
+    wrappingType="Normal"
+>
+Completed Bachelor of Engineering with First class
+    <br />
+    Stream- Information Technology
+</NotificationListItem>
+
+</NotificationList>
 
                 </ObjectPageSection>
 
@@ -336,14 +547,57 @@ const DetailPage = () => {
                     aria-label={intl.formatMessage({ id: 'Detail.LANG' })}
                     id="Lang"
                     titleText={intl.formatMessage({ id: 'Detail.LANG' })} >
-
+                        <br/>
+                      <FlexBox  direction="Row" justifyContent="Start"  wrap="NoWrap" alignItems="Center">
+                        <Title>English</Title>
+<RatingIndicator about='Test' value={5} max={5} readonly={true} ></RatingIndicator></FlexBox>
+<FlexBox  direction="Row" justifyContent="Start"  wrap="NoWrap" alignItems="Center">
+                        <Title>Marathi</Title>
+<RatingIndicator about='Test' value={5} max={5} readonly={true} ></RatingIndicator></FlexBox>
+<FlexBox  direction="Row" justifyContent="Start"  wrap="NoWrap" alignItems="Center">
+                        <Title>Hindi</Title>
+<RatingIndicator about='Test' value={5} max={5} readonly={true} ></RatingIndicator></FlexBox>
                 </ObjectPageSection>
 
                 <ObjectPageSection
                     aria-label={intl.formatMessage({ id: 'Detail.MYTIME' })}
                     id="Mytime"
                     titleText={intl.formatMessage({ id: 'Detail.MYTIME' })} >
+                    <PieChart
+                        dataset={[
+                            {
+                                name: 'Project Deliverables  Architecting and Developing',
+                                users: 50
+                            },
+                            {
+                                name: 'Communication with Stakeholders',
+                                users: 45
+                            },
+                            {
+                                name: 'Firm Contribution  Asset/Proposals',
+                                users: 15
+                            },
+                            {
+                                name: 'Team Management',
+                                users: 12
+                            },
+                            {
+                                name: 'Mentoring',
+                                users: 8
+                            },
+                            {
+                                name: 'Continuous self-improvement and Developmet Activities',
+                                users: 20
+                            }
+                        ]}
+                        dimension={{
+                            accessor: 'name'
+                        }}
+                        measure={{
+                            accessor: 'users'
+                        }}
 
+                    />
                 </ObjectPageSection>
 
 
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,47 +2,60 @@ import {
     ShellBar, Button, SideNavigationItem, SideNavigation
 } from '@ui5/webcomponents-react';
 import React, { useState } from "react";
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import "@ui5/webcomponents-icons/dist/arrow-left";
 import "@ui5/webcomponents-icons/dist/arrow-right";
-import "@ui5/webcomponents-icons/dist/menu";
+import "@ui5/webcomponents-icons/dist/bullet-text";
 import "@ui5/webcomponents-icons/dist/trend-down";
+import "@ui5/webcomponents-icons/dist/time-entry-request";
 
 import logo from "./logo.png";
 
-import {useIntl} from 'react-intl';
+import { useIntl } from 'react-intl';
 
 
 
- const MenuBar = () => {
+const MenuBar = () => {
     const [open, setOpen] = useState(false);
+
     const intl = useIntl();
 
 
     const navigate = useNavigate();
+
+    const updateSelected = (seq) => {
+        setMessage(newMessage);
+    };
+
     return (
         <>
-        
-            <ShellBar className="ui5-shellbar" logo={<img alt='SAP Certified'  src="https://sap.github.io/ui5-webcomponents/images/sap-logo-svg.svg"></img>}
-             
-             primaryTitle="Certified"
-             startButton={<Button icon="menu" id="toggle" onClick={() => { setOpen(open ? false : true) }}></Button>} >
-             
-                
+
+            <ShellBar className="ui5-shellbar" logo={<img src="https://sap.github.io/ui5-webcomponents/images/sap-logo-svg.svg"></img>}
+
+                primaryTitle="Certified"
+                startButton={<Button icon="bullet-text" id="toggle" onClick={() => { setOpen(open ? false : true) }}></Button>} >
+
+
             </ShellBar>
 
             <SideNavigation className="ui5-side-navigation" collapsed={open}>
 
-                <SideNavigationItem text="" icon="arrow-right" selected onClick={()=>{  navigate("/debit");}}></SideNavigationItem> 
+                <SideNavigationItem text="Summary" icon="arrow-right" selected onClick={() => { navigate("/debit"); }}></SideNavigationItem>
 
-                <SideNavigationItem text="" icon="arrow-left" onClick={()=>{navigate("/credit");}}></SideNavigationItem> 
+                <SideNavigationItem text="Skills" icon="arrow-left" onClick={() => { navigate("/credit"); }}></SideNavigationItem>
 
-              <SideNavigationItem text="" icon="trend-down" onClick={()=>{navigate("/history");}}></SideNavigationItem>
+                <SideNavigationItem text="Certifications" icon="trend-down" onClick={() => { navigate("/history"); }}></SideNavigationItem>
+
+                <SideNavigationItem text="Employment" icon="trend-down" onClick={() => { navigate("/history"); }}></SideNavigationItem>
+                <SideNavigationItem text="Projects" icon="trend-down" onClick={() => { navigate("/history"); }}></SideNavigationItem>
+                <SideNavigationItem text="Education" icon="trend-down" onClick={() => { navigate("/history"); }}></SideNavigationItem>
+                <SideNavigationItem text="Launguage" icon="trend-down" onClick={() => { navigate("/history"); }}></SideNavigationItem>
+                <SideNavigationItem text="My Time" icon="time-entry-request" onClick={() => { navigate("/history"); }}></SideNavigationItem>
 
             </SideNavigation>
         </>
-);
+    );
 
 }
 
